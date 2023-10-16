b67ba5cd5ff421b9f791668ec3eae1c9d8dc43e8
TheLuckyPerson <tylerle122002@gmail.com>
2023-10-15 20:47:56 -0700

    finished tdd

--- sample-functions.js
+++ sample-functions.js
@@ -1,5 +1,62 @@
-function mySum(a, b) {
-  return a + b;
-}
+class Profile {
+  stocks = [];
 
-export default { mySum };
+  isEmpty() {
+    return this.stocks.length == 0;
+  }
+
+  purchase(name, amt) {
+    let found_stock = this.findStock(name);
+    
+    if(found_stock != null) {
+          found_stock.tickers += amt;
+    } else {
+      let stock = new Stock(name, amt);
+      this.stocks.push(stock);
+    }
+  }
+
+  findStock(name) {
+    let found_stock = null;
+    for(let stock_idx = 0; stock_idx < this.stocks.length; stock_idx++) {
+      if(this.stocks[stock_idx].name == name) {
+        found_stock = this.stocks[stock_idx];
+        return found_stock;
+      }
+    }
+
+    return null;
+  }
+
+  getStockAmt(name) {
+    let found_stock = this.findStock(name);
+    if(found_stock != null) return found_stock.tickers;
+    return 0;
+  }
+
+  sell(name, amt) {
+    let found_stock = this.findStock(name);
+    
+    if(found_stock != null) {
+      found_stock.tickers -= amt;
+
+      if(found_stock.tickers == 0) {
+        this.stocks.splice(found_stock);
+      } else if (found_stock.tickers < 0) {
+        throw new Error("ShareSaleException");
+      }
+    }
+  }
+} 
+
+class Stock {
+  name = "";
+  tickers = 0;
+
+  constructor(name, ticks) {
+    this.name = name;
+    this.tickers = ticks;
+  }
+};
+
+export default Profile;
--- sample-functions.test.js
+++ sample-functions.test.js
@@ -1,7 +1,55 @@
-import myFunctions from "./sample-functions";
+import Profile from "./sample-functions.js";
+import Stock from "./sample-functions.js";
 
-test("Testing mySum -- success", () => {
-  const expected = 30;
-  const result = myFunctions.mySum(12, 18);
-  expect(expected).toBe(result);
+let profile;
+
+beforeEach(() => {
+  profile = new Profile();
+});
+
+test("No Shares", () => {
+  expect(profile.isEmpty()).toBeTruthy();
+});
+
+
+test("Has 2 shares", () => {
+  profile.stocks.push(new Stock("GME", 2));
+  profile.stocks.push(new Stock("RBLX", 2));
+
+  expect(profile.stocks.length).toBe(2);
+});
+
+test("Make a purchase", () => {
+  profile.purchase("GME", 5)
+  expect(profile.stocks[0].tickers).toBe(5);
+
+  profile.purchase("GME", 2)
+  expect(profile.stocks[0].tickers).toBe(7);
+});
+
+test("Make a sale", () => {
+  profile.purchase("GME", 6)
+  profile.sell("GME", 5)
+  expect(profile.stocks[0].tickers).toBe(1);
+});
+
+test("Check Amt", () => {
+  profile.purchase("GME", 2)
+  expect(profile.getStockAmt("GME")).toBe(2);
+});
+
+test("No Empty Stocks", () => {
+  profile.purchase("GME", 2)
+  profile.sell("GME", 2)
+  for(let stock_idx = 0; stock_idx < profile.stocks; stock_idx++) {
+    expect(profile.stocks[stock_idx].tickers).toNotBe(0);
+  }
+});
+
+test("No Over Sell", () => {
+  profile.purchase("GME", 2)
+  expect(() => {
+    profile.sell("GME", 3)
+  }).toThrow("ShareSaleException");
+
 });
